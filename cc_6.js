a610c313a0ce26549c629e6be108141e2cf1999a
Bartosz <bziolkowski@usf.edu>
2025-10-04 20:59:52 -0400

    Second commit which adds steps two through four.

--- cc_6.js
+++ cc_6.js
@@ -1,3 +1,43 @@
 // Coding Challenge 00
 
 // Code goes here
+
+// Step 1:
+
+// Step 2:
+
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    describe(){
+        return`${this.name} is an employee in ${this.department}`
+    }
+}
+
+// Step 3:
+
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe(){
+        return`${this.name} manages ${this.teamSize} people in ${this.department}`
+    }
+}
+
+// Step 4:
+
+let emp1 = new Employee("Raiden", "Sales");
+let emp2 = new Employee("Roxas", "Marketing");
+let mgr1 = new Manager("Zack", "Sales", 8);
+let mgr2 = new Manager("Riku", "IT", 6);
+
+console.log(emp1.describe());
+console.log(emp2.describe());
+console.log(mgr1.describe());
+console.log(mgr2.describe());
